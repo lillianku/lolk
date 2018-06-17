aa374968aef6c73ae85d5b7f96505d4ab76af06e
Lilli Ku <lilli.c.ku@disney.com>
2018-06-17 15:37:48 -0700

    renders champions on page

--- lolk/champions/urls.py
+++ lolk/champions/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('champions/', views.champions, name='champions'),
 ]
--- lolk/champions/views.py
+++ lolk/champions/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+def champions(request):
+    url = 'http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json'
+    response = requests.get(url)
+    champions = response.json()['data']
+    context = {
+        'champions': champions
+    }
+    return render(request, 'champions.html', context)
