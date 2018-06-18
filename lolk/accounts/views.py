c245701e023f3ae9c4f70edb25ffe2660741566a
Lilli Ku <lilli.c.ku@disney.com>
2018-06-18 13:14:07 -0700

    logs out

--- lolk/accounts/views.py
+++ lolk/accounts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import SignUpForm
+from django.contrib import auth
 
 # Create your views here.
 def signup(request):
@@ -22,4 +23,22 @@ def signup(request):
     return render(request, 'signup.html', context)
 
 def login(request):
-    return render(request, 'login.html',{'title':'Please Login'})
+    if request.method == 'POST':
+        user = auth.authenticate(
+        username=request.POST['username'],
+        password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            context = {
+                'title':'Something Went Wrong',
+                'error':'Username or Password Invalid'
+            }
+            return render(request, 'login.html', context)
+    return render(request, 'login.html', {'title':'Please Login'})
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
