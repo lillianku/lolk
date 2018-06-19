8b3d0fa78a74f778ace58e56e29673cda77de806
Lilli Ku <lilli.c.ku@disney.com>
2018-06-19 11:16:30 -0700

    builds champions model

--- champions/models.py
+++ champions/models.py
@@ -1,4 +1,10 @@
-# from django.db import models
-# from django.utils import timezone
-#
-# class Match(models.Model):
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Match(models.Model):
+    user_choice = models.CharField(max_length=100)
+    comp_choice = models.CharField(max_length=100)
+    result = models.CharField(max_length=10)
+    player = models.ForeignKey(User, on_delete=models.CASCADE)
+    battle_date = models.DateTimeField(default=timezone.now)
--- core/settings.py
+++ core/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'book_rater_db',
+        'NAME': 'champions',
         'USER':'postgres',
         'PASSWORD':'root',
         'HOST':'localhost',
